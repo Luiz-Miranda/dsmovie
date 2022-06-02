0b4692b89007dad31049e2eca0a1f08bb28f6477
Luiz-Miranda <luizline@hotmail.com>
2022-06-02 10:40:15 -0300

    Nome,Git

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,11 +5,11 @@ function Navbar() {
         <header >
             <nav className="container">
                 <div className="dsmovie-nav-content" >
-                    <h1>DsMovie</h1>
-                    <a href="https://github.com/devsuperior">
+                    <h1>LuizFilmes</h1>
+                    <a href="https://https://github.com/Luiz-Miranda">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
-                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                            <p className='dsmovie-contact-link'>/Luiz-Miranda</p>
                         </div>
                     </a>
                 </div>
